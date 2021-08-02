a3d454f47798a28b31d3e4058ca32e957ff78500
Alexander <sligxiii@gmail.com>
2021-08-02 18:14:53 +0300

    Removed dead classes.

--- src/Exception/Util/OptionsValidatorException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Exception\Util;
-
-use App\Contracts\Util\Exception\OptionsValidatorExceptionInterface;
-
-class OptionsValidatorException extends \RuntimeException implements OptionsValidatorExceptionInterface
-{
-}
